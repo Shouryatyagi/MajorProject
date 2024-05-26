a70e6046d1f0c82e8b743bce6a73deced2d2d6da
Shourya <shouryatyagi200@@gmail.com>
2024-05-26 16:51:46 +0530

    fial

--- static/recommend.js
+++ static/recommend.js
@@ -84,7 +84,7 @@ function movie_recs(movie_title,movie_id,my_api_key){
     },
     error: function(){
       alert("error recs");
-      $("#loader").delay(500).fadeOut();
+      $("#loader").delay(700).fadeOut();
     },
   }); 
 }
